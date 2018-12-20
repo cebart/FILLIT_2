1a232a2294a05da0c1e2868cf4f56a9695583502
cebart <33933281+cebart@users.noreply.github.com>
2018-12-20 16:49:27 +0100

    Add files via upload

--- check.c
+++ check.c
@@ -1,35 +1,6 @@
 #include "fillit.h"
 #include "get_next_line.h"
 
-/* GNL tout le fichier
- str = concat des 5 1er lignes puis CHECK (test si le retour de GNL fait bien strlen() == 3
- boucle sur les 5 lignes suivante... */
-
- /* CHECK : boucle sur les 4 premiers char de str & test si '.' ou '#'
- pareil pour les suivants
- si ok test si les 2 derniers sont '\n' et '\0' */
-
- /* /!\ GNL et '\n'  -> add compteur ( 1 tetri = 5 lignes )  || check GNL return ? si ligne vide == line vide ? */
-
-/*
-
-char    *str;
-char    *line;
-while (GNL)
-    {
-            i = 0;
-            while (i < 4)
-            {
-                GNL + concat (str + line)
-            }
-            test pour i == 4 si GNL retourne chaine vide
-
-            CHECK str
-            {
-            }
-            ft_strdel(str);
-    }
-*/
 int     check_format(const int fd)
 {
     int     bool_line;                          /* 1 pour line non vide sinon 0 -> test si 2 saut de ligne */
@@ -37,8 +8,9 @@ int     check_format(const int fd)
     char    *line;
     int     i;
 
-    cpt = 0;
     bool_line = 0;
+    cpt = 0;
+    line = ft_strnew(BUFF_SIZE);
     while (get_next_line(fd, &line) == 1)
     {
         i = 0;
