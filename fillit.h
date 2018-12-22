b47f6504b7124e6e17cbbde0e235eed9047bfd58
cebart <33933281+cebart@users.noreply.github.com>
2018-12-22 16:49:37 +0100

    Add files via upload

--- fillit.h
+++ fillit.h
@@ -8,5 +8,8 @@
 #include <stdio.h>
 
 int     check_file(const int fd);
+int     get_tetri(const int fd);
+char    **ft_2tabnew(int nbl, int nbc);
+char    ***ft_tab2tabnew(int nbtab, int nbl, int nbc);
 
 #endif
--- tetri.c
+++ tetri.c
@@ -1,1 +1,76 @@
 #include "fillit.h"
+
+
+char        ***ft_tab2tabnew(int nbtab, int nbl, int nbc)
+{
+    char    ***tab;
+	int     i;
+
+	i = 0;
+	if (!(tab = (char ***)ft_memalloc(sizeof(char **) * (nbtab + 1))))
+        return (0);
+	while (i < nbtab)
+	 {
+        tab[i] = ft_2tabnew(nbl, nbc);
+		i++;
+	}
+	tab[i] = '\0';
+	return (tab);
+}
+
+char        **ft_2tabnew(int nbl, int nbc)
+{
+    char    **tab;
+	int     i;
+
+    i = 0;
+    if (!nbl || !nbc)
+        return (NULL);
+    if (!(tab = (char **)malloc(sizeof(char *) * (nbl + 1))))
+         return (0);
+    while(i < nbl)
+    {
+        tab[i] = (ft_strnew(nbc + 1));
+        i++;
+    }
+    tab[i] = '\0';
+	return (tab);
+}
+
+int     get_tetri(const int fd)
+{
+    char    ***tetris;                                                   /* tetris[nb_tetris][largeur][longueur] / nb_tetris a recup*/
+    int     i;
+    int     j;
+    char    *line;
+
+    if (!(tetris = ft_tab2tabnew(4, 4, 4)))
+        return (0);
+    line = ft_strnew(4);
+    i = 0;
+    while (tetris[i])
+    {
+        j = 0;
+        while (j < 4)
+        {
+            get_next_line(fd, &line) == 1;
+            tetris[i][j] = line;
+            j++;
+        }
+        get_next_line(fd, &line) == 1;
+        i++;
+    }
+    i = 0;                              /* debug */
+	while (tetris[i] != NULL)
+	{
+        j = 0;
+        while (j < 4)
+        {
+            ft_putendl(tetris[i][j]);
+            j++;
+        }
+        ft_putchar('\n');
+        i++;
+	}                                           /* fin debug */
+    return (1);
+}
