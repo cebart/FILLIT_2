cf25989879cef349fcd048c7b36ce0bfd1f7adef
cebart <33933281+cebart@users.noreply.github.com>
2018-12-26 18:08:57 +0100

    Add files via upload

--- tetri.c
+++ tetri.c
@@ -1,6 +1,6 @@
 #include "fillit.h"
 
-int test_tetri(char **tetri)                     /* en cours  /!\ PB si le tetri se lit de droite a gauche ; ..## \n .##. /!\*/
+int     tetri_gauche(char **tetri)
 {
     int     i;
     int     j;
@@ -18,10 +18,67 @@ int test_tetri(char **tetri)                     /* en cours  /!\ PB si le tetri
             if (tmp[i][j] == '#')
             {
                 cpt++;
-                if ((i != 0 && tmp[i-1][j] != '.') || (i != 3 && tmp[i+1][j] != '.') || (j != 3 && tmp[i][j + 1] != '.') || (j != 0 && tmp[i][j - 1] != '.'))
+                if ((i != 0 && tmp[i-1][j] != '.') || (i != 3 && tmp[i+1][j] != '.') ||
+                    (j != 3 && tmp[i][j + 1] != '.') || (j != 0 && tmp[i][j - 1] != '.'))
                     tmp[i][j] = '.';
                 else
                     return (0);
+            }
+            j++;
+        }
+        i++;
+    }
+    return (1);
+}
+
+int     tetri_droite(char **tetri)
+{
+    int     i;
+    int     j;
+    int     cpt;
+    char    **tmp;
+
+    tmp = ft_2tabcpy(tetri);
+    i = 0;
+    cpt = 0;
+    while ( i < 4)
+    {
+        j = 3;
+        while (j > -1 && cpt < 3)
+        {
+            if (tmp[i][j] == '#')
+            {
+                cpt++;
+                if ((i != 0 && tmp[i-1][j] != '.') || (i != 3 && tmp[i+1][j] != '.') ||
+                    (j != 3 && tmp[i][j + 1] != '.') || (j != 0 && tmp[i][j - 1] != '.'))
+                    tmp[i][j] = '.';
+                else
+                    return (0);
+            }
+            j--;
+        }
+        i++;
+    }
+    return (1);
+}
+
+int     test_tetri(char **tetri)
+{
+    int     i;
+    int     j;
+
+    i = 0;
+    while ( i < 4)
+    {
+        j = 0;
+        while (j < 4)
+        {
+            if (tetri[i][j] == '#')
+            {
+                if ((tetri[i][j + 1] == '#' && tetri[i + 1][j] == '#'))
+                    return (0);
+                else
+                    return (1);
             }
             j++;
         }
@@ -38,6 +95,8 @@ char    **format_tetri(char **tetri, int c)
 
     bloc = 'A';
     i = 0;
+    if (c > 25)
+        return (NULL);
     while ( i < 4)
     {
         j = 0;
@@ -78,8 +137,16 @@ int     get_tetri(const int fd, int nbtetris)
     i = 0;
 	while (tetris[i] != NULL)
 	{
-            if(!test_tetri(tetris[i]))
-                return (0);
+            if (test_tetri(tetris[i]))
+            {
+                if(!tetri_gauche(tetris[i]))
+                    return (0);
+            }
+            else
+            {
+                if(!tetri_droite(tetris[i]))
+                    return (0);
+            }
             tetris[i] = format_tetri(tetris[i], i);
             i++;
 	}
