22887def09328304a9363fa208cbd9a35561d073
cebart <33933281+cebart@users.noreply.github.com>
2019-01-04 19:57:05 +0100

    Add files via upload

--- solve.c
+++ solve.c
@@ -1,54 +1,5 @@
 #include "fillit.h"
 
-int     is_free(char **tab, int lig, int col, char **tetri, int *cpt)           /* a mettre dans un fichier free.c */
-{
-    int i;
-    int j;
-
-    i = 0;
-    while (tab[lig] && tetri[i])
-    {
-        j = 0;
-        while (tab[lig][col] && tetri[i][j])
-        {
-            if (tetri[i][j] != '.' && tab[lig][col] != '.')
-                return (0);
-            else if (tetri[i][j] != '.')
-            {
-                tab[lig][col] = tetri[i][j];
-                (*cpt)++;
-            }
-            col++;
-            j++;
-        }
-        lig++;
-        i++;
-        col = col - j;
-    }
-    return (1);
-}
-
-int     is_free_and_place(char **res, char **tetri, int lig, int col)               /* a mettre dans un fichier free.c */
-{
-    int i;
-    int cpt;
-    char **tab;
-
-    tab = ft_2tabcpy(res);
-    cpt = 0;
-    if (!is_free(tab, lig, col, tetri, &cpt))
-        return (0);
-    if (cpt != 4)
-        return (0);
-    i = 0;
-    while (res[i])
-    {
-        strcpy(res[i], tab[i]);
-        i++;
-    }
-    return (1);
-}
-
 void    compute_2(char **res, char ***tetris, int t, char *placed)          /* 28 lignes */
 {
     int i;
